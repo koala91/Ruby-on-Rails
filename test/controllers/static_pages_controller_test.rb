308a6409b95f5cd9f64650be64184294db4a5ea6
xiewei <731390309@qq.com>
2019-06-16 16:26:58 +0800

    feat 使用具名路由

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,27 +7,27 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get home" do
-    get static_pages_home_url
+    get root_url
     assert_response :success
     # 重复的内容可以写一个函数
     assert_select "title", "home | Ruby on Rails" 
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "help | Ruby on Rails" 
   end
 
   # 我是失败的测试例子
   test "shoulid get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "about | Ruby on Rails" 
   end
 
   test "shoulid get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "contact | Ruby on Rails" 
   end
