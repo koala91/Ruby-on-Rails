7419b85c7f9faad38bde1c3fc131e03b8c7a099d
xiewei <xiewei@yeezon.com>
2019-06-20 03:05:34 +0800

    feat 7-signup-1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger 
+    # (控制台有效果)
   end
+  def new
+    # debugger
+    @user = User.new
+  end
+
+  def create
+    # 有漏洞,4.0开始不能用了
+    # @user = User.new(params[:user])
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
