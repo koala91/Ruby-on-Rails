baf068693310d286db54b8624e65a3f63142b1aa
xiewei <xiewei@yeezon.com>
2019-12-24 06:28:11 +0800

    feat

--- app/controllers/foo_controller.rb
+++
@@ -1,8 +0,0 @@
-class FooController < ApplicationController
-  # 想删除这个控制器的话 rails destory controller Foo bar baz
-  def bar
-  end
-
-  def baz
-  end
-end
--- app/helpers/users_helper.rb
+++
@@ -1,9 +0,0 @@
-module UsersHelper
-  def gravatar_for(user, size: 50)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    # size = options[:size]
-    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    # 有的邮箱才有图片
-    image_tag(gravatar_url, alt: user.name, class:"gravatar")
-  end
-end
