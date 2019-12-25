57ad992810efcb26bea67a0d586f072818c6cf1f
xiewei <xiewei@yeezon.com>
2019-12-26 02:36:00 +0800

    管理员

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 User.create!(name: "example User",
             email: "example@qq.com",
             password: "123456",
-            password_confirmation: "123456"
+            password_confirmation: "123456",
+            admin: true
             )
 
 99.times do |n|
