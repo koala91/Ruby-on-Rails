4c0c3db32d31f5a3afce5fb3f27dddfd83008f3d
xiewei <xiewei@yeezon.com>
2020-01-02 03:12:03 +0800

    发表微博1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   #   render html: "hello world2"
   # end
   include SessionsHelper
+
+
+  private
+    # 前置过滤器，确保用户已登录
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:dander] = '请登录'
+        redirect_to login_url
+      end
+    end
 end
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,2 +1,17 @@
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destory]
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = '发布成功'
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destory
+
+  end
 end
